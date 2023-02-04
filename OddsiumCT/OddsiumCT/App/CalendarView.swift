ad11f29af67df212e29cb74d01f005c359bbaf29
Johan Wejdenstolpe <johan.wejdenstolpe@bredband.net>
2023-02-04 13:01:54 +0100

    Setup the basics

---
+++ OddsiumCT/OddsiumCT/App/CalendarView.swift
@@ -0,0 +1,20 @@
+//
+//  CalendarView.swift
+//  OddsiumCT
+//
+//  Created by Johan Wejdenstolpe on 2023-02-04.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        Text("Calendar")
+    }
+}
+
+struct CalendarView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalendarView()
+    }
+}
